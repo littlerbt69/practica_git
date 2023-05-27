3631bb6bd0e228a0d50c61ba73e96f363ae15b04
rober <rober@DESKTOP-47GPGSQ>
2023-05-27 22:32:14 +0200

    push cuenta principal

--- Practica_git/src/practica/Ejercicio1.java
+++ Practica_git/src/practica/Ejercicio1.java
@@ -2,7 +2,9 @@ package practica;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-		
+	//cambio usuario principal
+	
+	
 	// Constantes
 	private static final int EDAD_MAXIMA = 100;
 	private static final int LIMITE_NINNO = 12;
